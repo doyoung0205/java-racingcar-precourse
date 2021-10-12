eb9c7dca7320bf1c91b39f59145685e837248fc0
김도영 <95doyoung@gmail.com>
2021-10-12 20:55:57 +0900

    feat(round): 사용하지 않는 클래스 삭제

--- src/main/java/racinggame/model/expcetion/InvalidRoundValueException.java
+++
@@ -1,8 +0,0 @@
-package racinggame.model.expcetion;
-
-public class InvalidRoundValueException extends RacingCarGameException {
-
-    public InvalidRoundValueException(String message) {
-        super(message);
-    }
-}

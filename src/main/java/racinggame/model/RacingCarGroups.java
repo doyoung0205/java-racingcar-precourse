f6e1defbe5d525c925d12c42568dc540bbd8a4a8
김도영 <95doyoung@gmail.com>
2021-10-13 13:46:53 +0900

    refactor(racingCarGroups): 같은 객체끼리 비교를 위해 equals, hashcode 메소드 추가

--- src/main/java/racinggame/model/RacingCarGroups.java
+++ src/main/java/racinggame/model/RacingCarGroups.java
@@ -73,6 +73,21 @@ public class RacingCarGroups {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RacingCarGroups carGroups = (RacingCarGroups) o;
+
+        return racingCars != null ? racingCars.equals(carGroups.racingCars) : carGroups.racingCars == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return racingCars != null ? racingCars.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         return racingCars.toString();
     }
